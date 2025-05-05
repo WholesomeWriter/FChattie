f25a3466ff5526ba6a78b4af72b7a3ac3174aafa
WholesomeWriter <wholesome_writer.proton.me>
2025-05-05 21:59:35 +0200

    Prepare auto-update pluin

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This template should help get you started developing with Tauri and SvelteKit in
 ## Recommended IDE Setup
 
 [VS Code](https://code.visualstudio.com/) + [Svelte](https://marketplace.visualstudio.com/items?itemName=svelte.svelte-vscode) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer).
+
+## F-list API documentation
+- https://wiki.f-list.net/Json_endpoints#API_Version_1 
+- Not sure how accurate it is : https://toys.in.newtsin.space/api-docs/#f-list-api
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,49 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri_plugin_updater::UpdaterExt;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
+        .setup(|app| {
+            let handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+              update(handle).await.unwrap();
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("error while running tauri application")
+        ;
 }
+
+async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+    let has_update = app.updater()?.check().await?;
+    match has_update {
+        Some(update) => {
+            let mut downloaded = 0;
+  
+            // alternatively we could also call update.download() and update.install() separately
+            update
+              .download_and_install(
+                |chunk_length, content_length| {
+                  downloaded += chunk_length;
+                  println!("downloaded {downloaded} from {content_length:?}");
+                },
+                || {
+                  println!("download finished");
+                },
+              )
+              .await?;
+        
+            println!("update installed");
+            app.restart();
+        }
+        None => {
+            println!("no update available");
+        }
+    }
+  
+    Ok(())
+  }
